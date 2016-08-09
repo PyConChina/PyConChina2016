37df7f281ddb81862b708f2eba92382f2b15b559
NsLib <mdl2009@vip.qq.com>
2016-08-09 23:14:37 +0800

    speakers.html

--- asset/js/menu.js
+++ asset/js/menu.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-console.log('~~~~~~');
+$(document).ready(function () {
   // fix menu when passed
+
   $('.masthead').visibility({
-      once: false,
-      onBottomPassed: function() {
-        $('.fixed.menu').transition('fade in');
-      },
-      onBottomPassedReverse: function() {
-        $('.fixed.menu').transition('fade out');
-      }
-    });
+    once: false,
+    onBottomPassed: function () {
+      $('.fixed.menu').transition('fade in');
+    },
+    onBottomPassedReverse: function () {
+      $('.fixed.menu').transition('fade out');
+    }
+  });
 
   // create sidebar and attach to menu open
   $('.ui.sidebar').sidebar('attach events', '.toc.item');
