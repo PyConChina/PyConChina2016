906bc3997876cb4d4b0b8595dbc8c4489e69ada1
NsLib <mdl2009@vip.qq.com>
2016-08-09 22:21:52 +0800

    跟staticjinja集成

--- bin/app.py
+++ bin/app.py
@@ -1,8 +1,57 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from staticpage import utils, gen
+from __future__ import print_function
+
+from multiprocessing import Process
+import os
+import signal
+import sys
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+from SocketServer import TCPServer
+
+import click
+
+from staticpycon import utils, gen
+
+
+def do_serve():
+    print('Listening on 0.0.0.0:8080 ...\n')
+    TCPServer.allow_reuse_address = True
+    server = TCPServer(('0.0.0.0', 8080), SimpleHTTPRequestHandler)
+    os.chdir(gen.SITE_DIR)
+    server.serve_forever()
+
+
+@click.command()
+@click.option('--debug', '-d', is_flag=True, default=False,
+              help=u'调试模式下不合并/压缩 Assets')
+@click.option('--generate', '-g', is_flag=True, default=False,
+              help=u'生成站点')
+@click.option('--serve', '-s', is_flag=True, default=False,
+              help=u'启动本地server')
+def run(debug, generate, serve):
+    utils.init_logger()
+
+    if generate and serve:
+        print(u'--generate和--serve不能同时使用')
+        sys.exit(1)
+
+    if serve:
+        p = Process(target=do_serve)
+
+        def signal_handler(sig, frame):
+            try:
+                p.terminate()
+            except Exception:
+                pass
+            sys.exit(0)
+
+        signal.signal(signal.SIGINT, signal_handler)
+        p.start()
+
+    gen.create_site(debug=debug, use_reloader=serve)
 
 
 if __name__ == '__main__':
-    utils.init_logger()
-    gen.create_site(debug=False, use_reloader=True)
+    run()
---
+++ bin/staticpycon/utils.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, print_function
+
+import errno
+import logging
+import os
+
+from ._colorama_compat import colorama
+
+
+_PROMPT_FMT_INVALID_ATTR = (
+    colorama.Style.DIM +
+    colorama.Fore.YELLOW +
+    'warn ' +
+    colorama.Style.RESET_ALL +
+    colorama.Fore.MAGENTA +
+    'invalid attribute ' +
+    colorama.Fore.RESET +
+    '%s'
+)
+
+_PROMPT_FMT_HTML = (
+    colorama.Style.DIM +
+    colorama.Fore.CYAN +
+    'html ' +
+    colorama.Fore.GREEN +
+    '[%s] ' +
+    colorama.Fore.RESET +
+    colorama.Style.RESET_ALL +
+    '%s'
+)
+
+
+# http://stackoverflow.com/a/600612/596531
+def mkdirp(path):
+    """``mkdir -p`` for Python."""
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def init_logger():
+    log = logging.getLogger('webassets')
+    log.addHandler(logging.StreamHandler())
+    log.setLevel(logging.DEBUG)
+
+
+def prompt_invalid_attr(fmts):
+    print(_PROMPT_FMT_INVALID_ATTR % fmts)
+
+
+def prompt_render_html(fmts):
+    print(_PROMPT_FMT_HTML % fmts)
