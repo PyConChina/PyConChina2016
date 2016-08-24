c497e2533c9d6cffbbc131fee72b6d58fc72bb03
TaoBeier <zhangjintao9020@gmail.com>
2016-08-24 10:30:06 +0800

    for deploy

---
+++ public/asset/js/app.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".masthead").visibility({once:false,onBottomPassed:function(){$(".fixed.menu").transition("fade in")},onBottomPassedReverse:function(){$(".fixed.menu").transition("fade out")}});$(".ui.sidebar").sidebar("attach events",".toc.item");$(".ui.menu .ui.dropdown").dropdown({on:"hover"})});
