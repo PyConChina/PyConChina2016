eb1a7631eb36f675d4a0aee07cfb48774009fe8f
NsLib <mdl2009@vip.qq.com>
2016-08-25 20:12:56 +0800

    修正深圳日程 & 加入捐赠者名单

---
+++ public/asset/webassets-external/4157a7081e12b0c3de1fdc99c7cb7090_menu.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+console.log('~~~~~~');
+  // fix menu when passed
+  $('.masthead').visibility({
+      once: false,
+      onBottomPassed: function() {
+        $('.fixed.menu').transition('fade in');
+      },
+      onBottomPassedReverse: function() {
+        $('.fixed.menu').transition('fade out');
+      }
+    });
+
+  // create sidebar and attach to menu open
+  $('.ui.sidebar').sidebar('attach events', '.toc.item');
+
+});
